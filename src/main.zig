afdf04c187b0040bfd44e877e5a0600306877c3c
WGrav01 <63617338+WGrav01@users.noreply.github.com>
2025-07-13 19:57:48 -0400

    main.zig with csv parsing for letter counting

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
+const letter = @import("letter.zig");
 
-pub fn main() void {
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
 
+    const letters = letter.getFirstLetterCount();
+
+    try stdout.print("letter,first,second,third,fourth,fifth\n", .{});
+    for (0..26) |i| {
+        try stdout.print("{c},{d},{d},{d},{d},{d}\n",
+            .{
+                letters.letter[i],
+                letters.first[i],
+                letters.second[i],
+                letters.third[i],
+                letters.fourth[i],
+                letters.fifth[i],
+            }
+        );
+    }
 }
