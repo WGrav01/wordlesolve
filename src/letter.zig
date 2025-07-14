4b83ce8ef2b662ce6e4e83fb2e6b3780aff46515
WGrav01 <63617338+WGrav01@users.noreply.github.com>
2025-07-13 20:45:42 -0400

    Cleared out switch statement

--- src/letter.zig
+++ src/letter.zig
@@ -2,21 +2,13 @@ const words = @import("words.zig").words;
 
 pub const Letters = struct {
     letter: [26]u8, // The letter itself
-    first: [26]u32, // How many times it appears as the first letter in a word
-    second: [26]u32, // How many times it appears as the second letter in a word
-    third: [26]u32, // How many times it appears as the third letter in a word
-    fourth: [26]u32, // How many times it appears as the fourth letter in a word
-    fifth: [26]u32, // How many times it appears as the fifth letter in a word
+    frequency: [26][5]u32,
 };
 
 pub fn getInitialLetterCount() Letters {
     var letters = Letters{
         .letter = undefined,
-        .first = [_]u32{0} ** 26,
-        .second = [_]u32{0} ** 26,
-        .third = [_]u32{0} ** 26,
-        .fourth = [_]u32{0} ** 26,
-        .fifth = [_]u32{0} ** 26,
+        .frequency = [26][5]u32{0} ** 26,
     };
 
     for (0..letters.letter.len) |i| {
@@ -26,16 +18,7 @@ pub fn getInitialLetterCount() Letters {
     for (words) |word| {
         inline for (0..5) |i| {
             const idx = word[i] - 'a';
-            if (idx < 26) {
-                switch (i) {
-                    0 => letters.first[idx] += 1,
-                    1 => letters.second[idx] += 1,
-                    2 => letters.third[idx] += 1,
-                    3 => letters.fourth[idx] += 1,
-                    4 => letters.fifth[idx] += 1,
-                    else => {},
-                }
-            }
+            letters.frequency[idx][i] += 1;
         }
     }
 
