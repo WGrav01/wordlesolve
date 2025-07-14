93c7cdcb06183dbe9363388ce3984003328f998f
WGrav01 <63617338+WGrav01@users.noreply.github.com>
2025-07-14 12:37:14 -0400

    Find first word

--- src/search.zig
+++ src/search.zig
@@ -1,3 +1,35 @@
 const words = @import("words.zig").words;
+const Letters = @import("letter.zig").Letters;
 
-pub fn findFirstWord() 
+pub fn findFirstWord(count: Letters) []const u8 {
+    var current_best: []const u8 = undefined;
+    var current_best_score: u32 = 0;
+    for (words) |word| {
+        var seen: u32 = 0;
+        var has_duplicate = false;
+        inline for (0..5) |i| {
+            const idx = word[i] - 'a';
+            if ((seen & (@as(u32, 1) << @as(u5, @intCast(idx)))) != 0) {
+                has_duplicate = true;
+                break;
+            }
+            seen |= (@as(u32, 1) << @as(u5, @intCast(idx)));
+        }
+
+        if (has_duplicate) continue; // skip words with duplicates
+
+        // Score word
+        var score: u32 = 0;
+        inline for (0..5) |i| {
+            const idx = word[i] - 'a';
+            score += count.frequency[idx][i];
+        }
+
+        if (score > current_best_score) {
+            current_best_score = score;
+            current_best = word;
+        }
+    }
+
+    return current_best;
+}
